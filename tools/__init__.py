aa018fb02a6a42932df79f2556ab64ad703d9279
Erik Huizinga <erik.huizinga@nedap.com>
2018-03-20 11:09:28 +0100

    Add documentation

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,7 +2,11 @@ import os
 
 
 def dot_to_pdf(dot_file_path: str, pdf_file_path: str, open_outfile: bool = False):
-    """Read a Graphviz DOT file and write to PDF."""
+    """Read a Graphviz DOT file and write to PDF. `dot` must be on your system path.
+    :param str dot_file_path: path to DOT file to read.
+    :param str pdf_file_path: path to PDF file to write.
+    :param bool open_outfile: open created PDF file if `True`; don't open it otherwise.
+    """
 
     os.system(f'dot {dot_file_path} -Tpdf > {pdf_file_path}')
 
