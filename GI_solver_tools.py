5164fd7afa22ebd958e3837094764df90579b7fc
martijn.slot <martijn.slot@nedap.com>
2018-03-21 10:15:16 +0100

    new build

--- GI_solver_tools.py
+++ GI_solver_tools.py
@@ -13,41 +13,25 @@ def write_file(G):
         write_dot(G)
 
 
-def solve(old: List[Graph], colorlist: list()):
-    new_graph = preprocess(old)
-    new_colornums_list = colorlist
-    new_colornums_list.append(get_colornums(new))
-    return is_omorph(h)
+def solve(G: list):
+    H = preprocess(G)
+    old_colorlist = get_colornums(H)
+    check_for_isomorphism(H, old_colorlist)
 
 
-def preprocess(g: List[Graph], colorlist: list()):
-    h = assign_colornums(g)
-    return h
-
-def get_colornums(g: Graph):
-    colorlist = []
-    for v in g.vertices:
-        colorlist.append(v.colornum)
-    return colorlist
+def preprocess(G):
+    processes_graph = assign_primary_colornums(G)
 
 
-def unique_colornums(g: Graph):
-    colornums = get_colornums(g)
-    if len(colornums) == len(set(colornums)):
-        return True
-    return False
+def assign_primary_colornums(G):
+    for graph in G:
+        for vertex in graph.vertices:
+            vertex.colornum = vertex.degree
 
 
-def assign_colornums(g: List[Graph]):
-    for graph in g:
-        if unique_colornums(graph):
-            return g
-        for v in graph.vertices:
-            if v.colornum is None:
-                v.colornum = v.degree
-    return g
+def get_colornums(H):
+    pass
 
 
-def is_omorph(g: List[Graph], list_colornums_old: list()):
-    assign_colornums()
-    list_colornums_old.append(get_colornums(graph))
+def check_for_isomorphism(H, old_colorlist):
+    pass
