35d55d93242adc7a07ac6f12f1e91c56aaa6ebcd
Erik Huizinga <erik.huizinga@nedap.com>
2018-03-21 17:31:34 +0100

    Reformat test code

--- test_claudia/unittest/coloring_claudia_test.py
+++ test_claudia/unittest/coloring_claudia_test.py
@@ -1,4 +1,5 @@
 import unittest
+
 from color_refiment_helper import *
 
 
@@ -23,9 +24,8 @@ class TestPr2(unittest.TestCase):
             if tail not in vertices:
                 vertices[tail] = Vertex(g, label=tail)
                 g.add_vertex(vertices[tail])
-            g.add_edge(Edge(vertices[head],vertices[tail]))
+            g.add_edge(Edge(vertices[head], vertices[tail]))
         return g
-
 
     def create_coloring_helper(self, graph: "Graph", map: "dict"):
         coloring = Coloring()
@@ -35,7 +35,6 @@ class TestPr2(unittest.TestCase):
                 coloring.set(key, vertex)
         return coloring
 
-
     def test_create_partition(self):
         g, vertices, coloring = self.helper(3)
         v_g1, v_g2, v_g3 = vertices
@@ -44,9 +43,9 @@ class TestPr2(unittest.TestCase):
         # v_g3 = Vertex(g)
         # coloring = {0: [v_g1, v_g2, v_g3]}
         new_coloring = create_partition(coloring, v_g1, v_g2)
-        self.assertEqual(2, new_coloring.num_colors) #__len__())
+        self.assertEqual(2, new_coloring.num_colors)  # __len__())
         self.assertListEqual([v_g1, v_g2], new_coloring.get(1))
-        self.assertListEqual([v_g3],       new_coloring.get(0))
+        self.assertListEqual([v_g3], new_coloring.get(0))
 
     def test_initialize_coloring(self):
         # empty graph
@@ -54,23 +53,23 @@ class TestPr2(unittest.TestCase):
         self.assertEqual(0, coloring.num_colors)
 
         # 1 - 2 - 3
-        g = self.create_graph_helper([[1,2],[2,3]])
+        g = self.create_graph_helper([[1, 2], [2, 3]])
         v_g1, v_g2, v_g3 = g.vertices
         coloring = initialize_coloring(g)
         self.assertEqual(2, coloring.num_colors)
         self.assertListEqual([v_g1, v_g3], coloring.get(1))
-        self.assertListEqual([v_g2],       coloring.get(2))
+        self.assertListEqual([v_g2], coloring.get(2))
         # 1 - 2 - 3
         #     |
         #     4 - 6 - 7
         #     |
         #     5
-        g = self.create_graph_helper([[1,2],[2,3],[2,4],[4,5],[4,6],[6,7]])
+        g = self.create_graph_helper([[1, 2], [2, 3], [2, 4], [4, 5], [4, 6], [6, 7]])
         v_g1, v_g2, v_g3, v_g4, v_g5, v_g6, v_g7 = g.vertices
         coloring = initialize_coloring(g)
         self.assertListEqual([v_g1, v_g3, v_g5, v_g7], coloring.get(1))
-        self.assertListEqual([v_g6],                   coloring.get(2))
-        self.assertListEqual([v_g2, v_g4],             coloring.get(3))
+        self.assertListEqual([v_g6], coloring.get(2))
+        self.assertListEqual([v_g2, v_g4], coloring.get(3))
         # 1 - 2 - 3
         #     |
         #     4 - 6 - 7
@@ -79,15 +78,15 @@ class TestPr2(unittest.TestCase):
         v_g8 = Vertex(g)
         g.add_vertex(v_g8)
         coloring = initialize_coloring(g)
-        self.assertListEqual([v_g8],                   coloring.get(0))
+        self.assertListEqual([v_g8], coloring.get(0))
         self.assertListEqual([v_g1, v_g3, v_g5, v_g7], coloring.get(1))
-        self.assertListEqual([v_g6],                   coloring.get(2))
-        self.assertListEqual([v_g2, v_g4],             coloring.get(3))
+        self.assertListEqual([v_g6], coloring.get(2))
+        self.assertListEqual([v_g2, v_g4], coloring.get(3))
 
     def test_has_same_color_neighbours(self):
-        g = self.create_graph_helper([[1,2],[2,3],[3,4],[3,5]])
+        g = self.create_graph_helper([[1, 2], [2, 3], [3, 4], [3, 5]])
         v_g1, v_g2, v_g3, v_g4, v_g5 = g.vertices
-        coloring = self.create_coloring_helper(g, {0: [1], 1: [4,5], 2: [2], 3:[3]})
+        coloring = self.create_coloring_helper(g, {0: [1], 1: [4, 5], 2: [2], 3: [3]})
 
         self.assertTrue(has_same_color_neighbours(v_g4, v_g5, coloring))
         self.assertFalse(has_same_color_neighbours(v_g1, v_g4, coloring))
@@ -122,21 +121,20 @@ class TestPr2(unittest.TestCase):
     #     self.assertFalse(no_bijection2.status == "Bijection")
 
     def test_choose_color(self):
-        graph = self.create_graph_helper([[1,2],[3,4],[5,6],[7,8]])
+        graph = self.create_graph_helper([[1, 2], [3, 4], [5, 6], [7, 8]])
         vertices = sorted(graph.vertices, key=Vertex.__str__)
 
         coloring = self.create_coloring_helper(graph, {0: [1, 2, 3, 4]})
         self.assertListEqual(vertices[:4], choose_color(coloring))
 
         coloring1 = self.create_coloring_helper(graph, {0: [1, 2],
-                     1: [1, 2, 3, 4],
-                     2: [1, 2, 3, 4, 5, 6]})
+                                                        1: [1, 2, 3, 4],
+                                                        2: [1, 2, 3, 4, 5, 6]})
         self.assertListEqual(vertices[:4], sorted(choose_color(coloring1), key=Vertex.__str__))
 
-
         coloring2 = self.create_coloring_helper(graph, {0: [1, 2],
-                     1: [1, 2, 3, 4, 5]})
-        self.assertListEqual([],           choose_color(coloring2))
+                                                        1: [1, 2, 3, 4, 5]})
+        self.assertListEqual([], choose_color(coloring2))
 
     def test_choose_vertex(self):
         g = Graph(False)
@@ -146,9 +144,9 @@ class TestPr2(unittest.TestCase):
         v_h = Vertex(h)
         self.assertEqual(v_g, choose_vertex([v_g, v_g2, v_h], g))
         self.assertEqual(v_h, choose_vertex([v_g, v_g2, v_h], h))
-        self.assertEqual(v_g, choose_vertex([v_h, v_g],       g))
+        self.assertEqual(v_g, choose_vertex([v_h, v_g], g))
         self.assertIsNone(choose_vertex([v_g, v_g2], h))
-        self.assertIsNone(choose_vertex([],          g))
+        self.assertIsNone(choose_vertex([], g))
 
     def test_get_vertices_of_graph(self):
         g = Graph(False)
@@ -157,10 +155,10 @@ class TestPr2(unittest.TestCase):
         v_g2 = Vertex(g)
         v_h = Vertex(h)
         self.assertListEqual([v_g, v_g2], get_vertices_of_graph([v_g, v_g2, v_h], g))
-        self.assertListEqual([v_h],       get_vertices_of_graph([v_g, v_g2, v_h], h))
-        self.assertListEqual([],          get_vertices_of_graph([v_h],       g))
-        self.assertListEqual([],          get_vertices_of_graph([v_g, v_g2], h))
-        self.assertListEqual([],          get_vertices_of_graph([],          g))
+        self.assertListEqual([v_h], get_vertices_of_graph([v_g, v_g2, v_h], h))
+        self.assertListEqual([], get_vertices_of_graph([v_h], g))
+        self.assertListEqual([], get_vertices_of_graph([v_g, v_g2], h))
+        self.assertListEqual([], get_vertices_of_graph([], g))
 
     def test_are_twins(self):
         g = Graph(False)
@@ -240,6 +238,7 @@ class TestPr2(unittest.TestCase):
         twins, false_twins = get_twins(g)
         self.assertListEqual([(v0, v2), (v0, v3), (v2, v3), (v6, v7)], twins)
         self.assertListEqual([], false_twins)
+
 
 if __name__ == '__main__':
     unittest.main()
