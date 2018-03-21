5ce70465da85baa963c8e6865f424ad967809ee1
martijn.slot <martijn.slot@nedap.com>
2018-03-21 14:15:35 +0100

    topdf works piemel

--- mastersolver.py
+++ mastersolver.py
@@ -1,6 +1,9 @@
 from GI_solver_tools import *
+import tools
 
 G = load_file('colorref_smallexample_2_49.grl')
 solve(G)
-# write_file(G)
-print_graph(G)
+for graph in G:
+    write_file(graph)
+
+tools.dot_to_pdf('colorful.dot', 'piemel.pdf', True)
