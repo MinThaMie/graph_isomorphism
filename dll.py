0ae415f604a98d1f43d4de84c92cdf96b31aa970
Dorien Meijer Cluwen <f.t.f.meijercluwen@student.utwente.nl>
2018-03-26 09:53:53 +0200

    Update double linked lists. (#29)
    * Ideas on double linked lists.
    
    Adds:
    - more documentation
    - some monkey testing
    - __str__ methods
    - O(1) push and pop methods
    - delete by value method
    - __len__ method
    - __iter__ and __reverse__ methods, now we can loop
    
    * Make dll generic
    
    * Add tests for DoublyLinkedList class
    
    Renames push and push_left to append and append_left
    Removes ‘val’ argument from constructor
    Fix some bugs
    
    * Fix tests
    
    * Remove ignored files
    
    * Improve dll
    
    Rename remove and delete (put them the wrong way around)
    Add __contains__ method (you can now do `if elem in dll then …`
    Fix __main__ in dll, for some monkey testing
    Fix paths in testfiles
    
    * Revert changes on graph paths in tests
    
    Apparently graphs live in different places in travis
    
    * Fix delete method
    
    Went wrong when you deleted the first or last node.
    Update tests
    
    * Use DLL in coloring

--- dll.py
+++ dll.py
@@ -1,54 +1,199 @@
-from graph import Vertex
+"""
+Module for Doubly Linked Lists
+"""
+#See for more info on generics in python https://docs.python.org/3/library/typing.html
+from typing import TypeVar
+T = TypeVar('T')
 
 
 class Node(object):
+    def __init__(self, value: T, prev: "Node" = None, next: "Node" = None):
+        self._value = value
+        self.prev = prev
+        self.next = next
 
-    def __init__(self, vertex: "Vertex", prev: "Vertex"=None, next: "Vertex"=None):
-        self._vertex = vertex
-        self._prev = prev
-        self._next = next
+    @property
+    def value(self) -> T:
+        return self._value
+
+    def __str__(self):
+        return str(self._value)
 
 
 class DoubleLinkedList(object):
-    def __init__(self, node: "Vertex"=None):
-        self._first_node = node
-        self._last_node = node
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._size = 0
 
-    def insert_after(self, node, new_node):
+    def append_left(self, val: T):
+        """Add element to the start of the list"""
+        new_node = Node(val)
+        if self._size == 0:
+            self._head = new_node
+            self._tail = new_node
+        else:
+            self._head.prev = new_node
+            new_node.next = self._head
+            self._head = new_node
+        self._size += 1
+
+    def append(self, val: T):
+        """Add element to the end of the list"""
+        new_node = Node(val)
+        if self._size == 0:
+            self._head = self._tail = new_node
+        else:
+            self._tail.next = new_node
+            new_node.prev = self._tail
+            self._tail = new_node
+        self._size += 1
+
+    def pop(self) -> T:
+        """Remove last node and return value"""
+        if self._size == 0:
+            return None
+        elif self._size == 1:
+            last_node = self._tail
+            self._head = self._tail = None
+        else:
+            last_node, self._tail = self._tail, self._tail.prev
+            self._tail.next = None
+        self._size -= 1
+        return last_node.value
+
+    def pop_left(self):
+        """Remove first node and return value"""
+        if self._size == 0:
+            return None
+        elif self._size == 1:
+            first_node = self._head
+            self._head = self._tail = None
+        else:
+            first_node, self._head = self._head, self._head.next
+            self._head.prev = None
+
+        self._size -= 1
+        return first_node.value
+
+    def insert_after(self, node: "Node", val: T):
+        new_node = Node(val)
         new_node.prev = node
         if node.next is None:
             new_node.next = None
-            self._last_node = new_node
+            self._tail = new_node
         else:
             new_node.next = node.next
             node.next.prev = new_node
         node.next = new_node
+        self._size += 1
 
-    def insert_before(self, node, new_node):
+    def insert_before(self, node: "Node", val: T):
+        new_node = Node(val)
         new_node.next = node
         if node.prev is None:
             new_node.prev = None
-            self._first_node = new_node
+            self._head = new_node
         else:
             new_node.prev = node.prev
             node.prev.next = new_node
         node.prev = new_node
+        self._size += 1
 
-    def append(self, node):
-        if self._first_node is None:
-            self._first_node = node
-            self._last_node = node
-            node.prev = None
-            node.next = None
-        else:
-            self.insert_after(self._last_node, node)
+    def remove(self, value: T):
+        """Remove node with given value if existing"""
+        node = self._head
+        while node is not None:
+            if node.value == value:
+                self.delete(node)
+                return
+            node = node.next
 
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+    def delete(self, node: "Node") -> T:
+        """Deletes and returns value of given node, connects previous and next"""
+        if node.value not in self:
+            return None
+
+        prev_node, next_node = node.prev, node.next
+        if prev_node is not None:
+            prev_node.next = next_node
+        if next_node is not None:
+            next_node.prev = prev_node
+
+        if node == self._head:
+            self._head = next_node
+        if node == self._tail:
+            self._tail = prev_node
         node.next = None
         node.prev = None
-        if self._first_node is node:
-            self._first_node = node.next
-        if self._last_node is node:
-            self._last_node = node.prev
+        self._size -= 1
+        return node.value
+
+    def find(self, value: T) -> "Node":
+        """Returns first node with given value, or None if no such node is found"""
+        node = self._head
+        while node is not None:
+            if node.value == value:
+                return node
+            node = node.next
+        return None
+
+    def __len__(self) -> int:
+        """Number of nodes in the doubly linked list"""
+        return self._size
+
+    def __str__(self):
+        """Nice representation, needs to loop over all elements!"""
+        txt = "["
+        node = self._head
+        while node is not None:
+            txt += str(node) + ','
+            node = node.next
+        txt = txt[:-1] + ']'
+        return txt
+
+    def __iter__(self):
+        """Allows forward looping like 'for i in dll'"""
+        node = self._head
+        while node:
+            yield node.value
+            node = node.next
+
+    def __reversed__(self):
+        """Allows reversed looping"""
+        node = self._tail
+        while node:
+            yield node.value
+            node = node.prev
+
+    def __contains__(self, item: T):
+        """Check if element is contained in the DoubleLinkedList
+        Now we can do `if elem in dll then <something> end`
+        :param item: Item of which we want to know if it is contained in the dll
+        :return: True if found, False otherwise
+        """
+        return (self.find(item) is not None);
+
+
+if __name__ == '__main__':
+    from graph import Vertex, Graph
+
+    g = Graph(False)
+    v = [Vertex(g) for i in range(5)]
+    n = Node(v[0])
+    lst = DoubleLinkedList()
+    lst.append([0])
+    lst.append_left(v[1])
+    lst.append(v[2])
+    print(type(lst))
+
+    for u in lst:
+        print(u)
+    print(lst)
+
+    lst2 = DoubleLinkedList()
+    lst2.append(1)
+    lst2.append(2)
+    lst2.append(2)
+    lst2.append_left(3)
+    print(lst2)
---
+++ tests/unit/test_dll.py
@@ -0,0 +1,239 @@
+"""
+Test file for Doubly Linked Lists
+"""
+import unittest
+from dll import DoubleLinkedList, Node
+
+
+class DoublyLinkedListCase(unittest.TestCase):
+    dll = None
+
+    def setUp(self):
+        self.dll = DoubleLinkedList()
+
+    def test_init(self):
+        self.assertIsNone(self.dll._head, "Initial HEAD should be None")
+        self.assertIsNone(self.dll._tail, "Initial TAIL should be None")
+        self.assertEqual(0, len(self.dll), "Initial length should be zero")
+
+    def test_append_left(self):
+        self.dll.append_left(1)
+        self.assertEqual(1, self.dll._head.value, "Expected value 1 in first Node")
+        self.assertEqual(1, len(self.dll), "Length should be 1")
+        self.assertEqual(self.dll._head, self.dll._tail, "HEAD should equal TAIL")
+
+        prev_added_node = self.dll._head
+        count = 1
+        for i in range(5):
+            self.dll.append_left(i)
+            count += 1
+
+            self.assertEqual(i, self.dll._head.value, "Expected value " + str(i) + " in first Node")
+            self.assertEqual(prev_added_node.value, self.dll._head.next.value,  "Expected value" + str(prev_added_node.value) + "in next Node")
+            self.assertEqual(count, len(self.dll), "Length should be " + str(count))
+            self.assertEqual(prev_added_node, self.dll._head.next, "First node should point to second node")
+            self.assertEqual(self.dll._head, prev_added_node.prev, "Second node should point to this node")
+            prev_added_node = self.dll._head
+
+    def test_append(self):
+        self.dll.append(1)
+        self.assertEqual(self.dll._head.value, 1, "Expected value 1 in first Node")
+        self.assertEqual(len(self.dll), 1, "Length should be 1")
+        self.assertEqual(self.dll._head, self.dll._tail, "HEAD should equal TAIL")
+
+        prev_added_node = self.dll._tail
+        count = 1
+        for i in range(5):
+            self.dll.append(i)
+            count += 1
+
+            self.assertEqual(self.dll._tail.value, i, "Expected value " + str(i) + " in last Node")
+            self.assertEqual(self.dll._tail.prev.value, prev_added_node.value,
+                             "Expected value" + str(prev_added_node.value) + "in previous Node")
+            self.assertEqual(len(self.dll), count, "Length should be " + str(count))
+            self.assertEqual(self.dll._tail.prev, prev_added_node, "Added node should point to second last node")
+            self.assertEqual(prev_added_node.next, self.dll._tail, "Second Last node should point to added node")
+            prev_added_node = self.dll._tail
+
+    def test_pop(self):
+        count = 5
+        for i in range(count):
+            self.dll.append(i)
+
+        self.assertEqual(count, len(self.dll), "Length should be " + str(count))
+
+        # Pop from a dll with > 1 elements
+        for i in range(count,1, -1):
+            value = self.dll.pop()
+            count -= 1
+
+            self.assertEqual(count, len(self.dll), "Length should be " + str(count))
+            self.assertEqual(i - 1, value, "Value should equal " + str(i - 1))
+            self.assertIsNone(self.dll._tail.next, "TAIL.next should be None")
+
+        # Pop from a dll with 1 element
+        self.assertEqual(count, len(self.dll), "Length should be " + str(1))
+        value = self.dll.pop()
+
+        self.assertEqual(0, len(self.dll),"Length should be " + str(0))
+        self.assertEqual(0, value, "Value should equal " + str(0))
+        self.assertIsNone(self.dll._head, "HEAD should be None")
+        self.assertIsNone(self.dll._tail, "TAIL should be None")
+
+        # Pop from an empty dll
+        node = self.dll.pop()
+        self.assertIsNone(node, "Popping an empty dll returns None")
+
+    def test_pop_left(self):
+        n = count = 5
+        for i in range(count):
+            self.dll.append(i)
+
+        self.assertEqual(count, len(self.dll), "Length should be " + str(count))
+
+        # Pop_left from a dll with > 1 elements
+        for i in range(count - 1):
+            value = self.dll.pop_left()
+            count -= 1
+
+            self.assertEqual(count, len(self.dll), "Length should be " + str(count))
+            self.assertEqual(i, value, "Value should equal " + str(i))
+            self.assertEqual(i + 1, self.dll._head.value, "Next value should equal " + str(i + 1))
+
+        # Pop from a dll with 1 element
+        self.assertEqual(count, len(self.dll), "Length should be " + str(1))
+        value = self.dll.pop_left()
+
+        self.assertEqual(0, len(self.dll), "Length should be " + str(0))
+        self.assertEqual(n - 1, value, "Value should equal " + str(n - 1))
+        self.assertIsNone(self.dll._head, "HEAD should be None")
+        self.assertIsNone(self.dll._tail, "TAIL should be None")
+
+        # Pop from an empty dll
+        node = self.dll.pop_left()
+        self.assertIsNone(node, "Popping an empty dll returns None")
+
+    def test_find(self):
+        n = 5
+        for i in range(n):
+            self.dll.append(i)
+
+        self.assertEqual(3, self.dll.find(3).value, "Can find value of 3")
+        self.assertIsNone(self.dll.find(10), "Cannot find a node with a value of 10")
+
+    def test_insert_after(self):
+        n, m = 5, 3
+        node = None
+        for i in range(n):
+            self.dll.append(i)
+
+        node = self.dll.find(m)
+        node2 = node.next
+        self.dll.insert_after(node, 10)
+        self.assertEqual(10, node.next.value, "Value of 10 is inserted after given node")
+        self.assertEqual(node.next, node2.prev, "New node is inserted before 'node2")
+
+        expected = [i for i in range(n)]
+        expected.insert(m+1, 10)
+        count = 0
+        for i in self.dll:
+            self.assertEqual(expected[count], i)
+            count += 1
+
+        # Insert after TAIL
+        self.dll.insert_after(self.dll._tail, 11)
+        self.assertEqual(11, self.dll._tail.value, "Value of 11 is inserted at TAIL")
+        self.assertIsNone(self.dll._tail.next, "HEAD.next is None")
+
+    def test_insert_before(self):
+        n, m = 5, 3
+        node = None
+        for i in range(n):
+            self.dll.append(i)
+
+        node = self.dll.find(m)
+        node2 = node.prev
+        self.dll.insert_before(node, 10)
+        self.assertEqual(10, node.prev.value, "Value of 10 is inserted before 'node'")
+        self.assertEqual(node2.next, node.prev, "New node is inserted after 'node2")
+
+        expected = [i for i in range(n)]
+        expected.insert(m, 10)
+        count = 0
+        for i in self.dll:
+            self.assertEqual(expected[count], i)
+            count += 1
+
+        # Insert before HEAD
+        self.dll.insert_before(self.dll._head, 11)
+        self.assertEqual(11, self.dll._head.value, "Value of 11 is inserted at HEAD")
+        self.assertIsNone(self.dll._head.prev, "HEAD.prev is None")
+
+    def test_remove(self):
+        n = 5
+        for i in range(n):
+            self.dll.append(i)
+
+        # Remove existing value
+        self.dll.remove(2)
+        self.assertEqual(n - 1, len(self.dll), "Length should be " + str(n - 1))
+        self.assertIsNone(self.dll.find(2), "There should be no value of 2 in the dll")
+        print(self.dll)
+
+        # Remove non existing value
+        self.dll.remove(-1)
+        self.assertEqual(n - 1, len(self.dll), "Length should still be" + str(n - 1))
+
+    def test_delete(self):
+        n = 5
+        for i in range(n):
+            self.dll.append(i)
+
+        # Delete existing value
+        node = self.dll.find(2)
+        prev = node.prev
+        next = node.next
+        val = self.dll.delete(node)
+        self.assertEqual(2, val, "Returned value should be 2")
+        self.assertEqual(n - 1, len(self.dll), "Length should be " + str(n - 1))
+        self.assertIsNone(self.dll.find(2), "There should be no value of 2 in the dll")
+        self.assertEqual(next, prev.next, "Links previous to next node")
+        self.assertEqual(prev, next.prev, "Links next node to previous")
+
+        # Delete non existing value
+        node = Node(2)
+        val = self.dll.delete(node)
+        self.assertIsNone(val, "Should return None")
+        self.assertEqual(n - 1, len(self.dll), "Length should still be " + str(n - 1))
+        print(self.dll)
+
+        # Delete first node
+        node = self.dll.find(0)
+        next = node.next
+        self.dll.delete(node)
+        self.assertEqual(len(self.dll), n-2,"Length should be " + str(n-2))
+        self.assertIsNone(self.dll.find(0), "There should be no value of 0 in the dll")
+        for elem in self.dll:
+            self.assertEqual(next.value, elem, "Next should now be at the HEAD")
+            break
+        print(self.dll)
+
+        # Delete last node
+        print(self.dll)
+        node = self.dll.find(n - 1)
+
+
+    def test_contains(self):
+        n = 5
+        for i in range(n):
+            self.dll.append(i)
+
+        # 0 - 4 should be contained in dll
+        for i in range(n):
+            self.assertTrue(i in self.dll, "DLL should contain " + str(i))
+
+        # Should not contain n
+        self.assertFalse(n in self.dll, "DLL should not contain" + str(n))
+
+if __name__ == '__main__':
+    unittest.main()
