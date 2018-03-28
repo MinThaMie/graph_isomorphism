ccb8a8d956320e67b7ac8f75645c2706d8b9cbbd
Mark Banierink <mark.banierink@nedap.com>
2018-03-28 14:09:07 +0200

    environment geupdate

--- environment.py
+++ environment.py
@@ -2,6 +2,8 @@ import os
 
 import time
 
+import datetime
+
 from color_refinement import get_number_automorphisms, is_isomorphisms, count_isomorphism
 from graph_io import load_graph
 
@@ -9,6 +11,7 @@ PATH_BRANCHING = 'graphs/branching'
 PATH_COLORREF = 'graphs/colorref'
 PATH_TREEPATHS = 'graphs/treepaths'
 OUTPUT_FORMAT = '{:10} {:>15} {:>25} {:>15} {:>25} {:>25}'
+OUTPUT_PATH = 'export/result'
 
 
 def main():
@@ -37,24 +40,19 @@ def get_graphs_from_file(file):
 
 
 def process_graphs(file, graphs):
-    export(create_titles(file))
+    result = create_titles(file)
     for i in range(len(graphs)):
+        iso_start = time.time()
+        isomorphs = get_isomorphs(graphs)
+        iso_time = time.time() - iso_start
         auto_start = time.time()
         automorphs = get_number_automorphisms(graphs[i])
         auto_time = time.time() - auto_start
-        isomorphs = []
-        iso_start = time.time()
-        for j in range(len(graphs)):
-            if j > i:
-                isomorph = is_isomorphisms(graphs[i], graphs[j])
-                num = count_isomorphism(graphs[i], graphs[j])
-                #TODO gaat het om de totale serie vergelijken? Want dan kan er efficiënter gekozen worden: niet alle grafen vergelijken, maar alleen die nog niet vergeleken zijn. Als de rest al iso/auto (wat is wat?) is, zijn die dus sowieso al wel/niet gelijk aan elkaar
-                print(graphs[i].name, 'and', graphs[j].name, 'isomorphic?', isomorph)
-                print('There are', num, 'isomorphisms')
-
-        iso_time = time.time() - iso_start
         total_time = auto_time + iso_time
-        export(OUTPUT_FORMAT.format(graphs[i].name, automorphs, auto_time, isomorphs, iso_time, total_time))
+        subresult = OUTPUT_FORMAT.format(graphs[i].name, isomorphs, iso_time, automorphs, auto_time, total_time)
+        result += result + '\n' + subresult
+        print(subresult)
+    export_result(result)
 
 
 def create_titles(file):
@@ -66,8 +64,10 @@ def create_titles(file):
     return title
 
 
-def export(string):
-    print(string)
+def export_result(result):
+    output_file = open('result_' + str(round(time.time())), 'w')
+    output_file.write(result)
+    output_file.close()
 
 
 if __name__ == "__main__":
