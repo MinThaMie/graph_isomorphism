f7ca44b07e8a5e1df9cc0b247f81ac5d4cfcc610
Claudia Reuvers <>
2018-03-28 14:49:50 +0200

    Fixed integration test with files
    * Added small selection of graphs to test (otherwise takes very long)
    * put printing back: can see if indeed checkings have been taken place

--- tests/unit/test_count_isomorphism_dorien.py
+++ tests/unit/test_count_isomorphism_dorien.py
@@ -7,24 +7,23 @@ import unittest
 from color_refinement import get_number_isomorphisms
 from graph_io import *
 
-PATH = 'graphs/branching'
-EXPECTED = dict()
+PATH = '../../graphs/branching'
 
 
-def expected_result(filename, g_name, h_name):
+def expected_result(filename, g_name, h_name, expected):
     key = g_name + h_name
-    if key in EXPECTED[filename].keys():
-        return EXPECTED[filename][key]
+    if key in expected[filename].keys():
+        return expected[filename][key]
     else:
         return 0
 
 
-def get_files():
+def get_files(expected: dict):
     all_graphs = os.listdir(PATH)
-    return [x for x in all_graphs if x in EXPECTED.keys()]
+    return [x for x in all_graphs if x in expected.keys()]
 
 
-def testfile(filename):
+def testfile(filename, file_expected):
     """Check if results for the given file are correct"""
     with open(PATH + "/" + filename) as f:
         L = load_graph(f, read_list=True)
@@ -35,7 +34,7 @@ def testfile(filename):
         for j in range(len(graphs)):
             if j > i:
                 num = get_number_isomorphisms(graphs[i], graphs[j], True)
-                expected = expected_result(filename, graphs[i].name, graphs[j].name)
+                expected = expected_result(filename, graphs[i].name, graphs[j].name, file_expected)
                 message = "Expected " + str(expected) + " for " + graphs[i].name + " and " + graphs[
                     j].name + " in " + filename
                 results.append([expected, num, message])
@@ -45,17 +44,20 @@ def testfile(filename):
 class CountIsomorphismCase(unittest.TestCase):
     """Tests for `color_refinement.py`."""
 
+    EXPECTED = None
+
+    def setUp(self):
+        with open(PATH + "/" + 'expected_results_small.txt') as f:
+            self.EXPECTED = read_expected_result(f)
+
     def test_files(self):
-        files = get_files()
+        files = get_files(self.EXPECTED)
         for file in files:
-            results = testfile(file)
+            results = testfile(file, self.EXPECTED)
             for result in results:
                 self.assertEqual(result[0], result[1], result[2])
-                # print(result[2], 'got', result[1])
+                print(result[2], 'got', result[1])
 
 
 if __name__ == '__main__':
-    with open(PATH + "/" + 'expected_results.txt') as f:
-        EXPECTED = read_expected_result(f)
-
     unittest.main()
