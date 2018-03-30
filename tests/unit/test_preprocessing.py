ce247c8f73a58df0442510fccf084ca6d02bee79
MartijnSlot <martijn.slot@nedap.com>
2018-03-30 11:31:50 +0200

    Preprocess added to refinement and tests (#40)
    * added preprocess to color_refinement and tests
    
    * blaat
    
    * incl new gitignore

---
+++ tests/unit/test_preprocessing.py
@@ -0,0 +1,25 @@
+import unittest
+import preprocessing
+from tests import *
+
+
+class TestPreprocessing(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.v4e4 = graph_vertex4edge4()
+        cls.v5e4loop = graph_vertex5edge4loop()
+
+    def test_checks(self):
+        g = self.v4e4
+        h = self.v4e4
+        self.assertTrue(preprocessing.checks(g, h))
+
+        g = self.v5e4loop
+        self.assertFalse(preprocessing.checks(g, h))
+
+    def test_loner_removal(self):
+        g = self.v5e4loop  # g has 1 'loner'
+        num_vertices = len(g.vertices)
+        g = preprocessing.remove_loners(g)
+        self.assertEqual(num_vertices - 1, len(g.vertices))
