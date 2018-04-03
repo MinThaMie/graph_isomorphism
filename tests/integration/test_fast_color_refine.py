8e40cdeefef1acbdc1ccdbe1c0ca48e75964a74c
Erik Huizinga <erik.huizinga@nedap.com>
2018-04-03 18:05:14 +0200

    Reformat test_fast_color_refine.py

--- tests/integration/test_fast_color_refine.py
+++ tests/integration/test_fast_color_refine.py
@@ -7,13 +7,13 @@ import unittest
 from color_refinement import fast_color_refine
 from color_refinement_helper import *
 from graph_io import *
-from tools import dot_to_pdf
 
-PATH = 'graphs/colorref'# to run locally from PyCharm: PATH = '../../graphs/colorref'
+PATH = 'graphs/colorref'
 EXPECTED = dict()
 EXPECTED['colorref_smallexample_2_49.grl'] = {'G0G1': "Bijection"}
 EXPECTED['colorref_smallexample_4_7.grl'] = {'G1G3': "Bijection", 'G0G2': None}
 EXPECTED['colorref_smallexample_6_15.grl'] = {'G0G1': "Bijection", 'G2G3': None, 'G4G5': None}
+
 
 def get_expected_result(filename, g_name, h_name):
     if filename in EXPECTED.keys():
@@ -23,9 +23,11 @@ def get_expected_result(filename, g_name, h_name):
         else:
             return "Unbalanced"
 
+
 def get_color_ref_files():
     all_graphs = os.listdir(PATH)
     return [x for x in all_graphs if x in EXPECTED.keys()]
+
 
 def testfile(filename):
     """Check if results for the given file are correct"""
@@ -46,6 +48,7 @@ def testfile(filename):
                 results.append([expected, status, message])
     return results
 
+
 class FastColorRefineCase(unittest.TestCase):
     """Tests for `color_refinement.py`."""
 
@@ -56,6 +59,7 @@ class FastColorRefineCase(unittest.TestCase):
             for result in results:
                 self.assertEqual(result[0], result[1], result[2])
                 debug(result[2], 'got', result[1])
+
 
 if __name__ == '__main__':
     unittest.main()
