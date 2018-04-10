66cf131e8306a5eaa4cd4de1f64b63ee5e1fdbd6
Erik Huizinga <erik.huizinga@nedap.com>
2018-04-10 09:58:38 +0200

    bonusGI1.grl

--- environment.py
+++ environment.py
@@ -15,7 +15,7 @@ TREEPATHS = os.path.join(GRAPHS, 'treepaths')
 D_DAY = os.path.join(GRAPHS, 'd_day')
 BASIC = os.path.join(D_DAY, 'basic/')
 
-FILE = ''
+FILE = os.path.join(D_DAY, 'bonusGI1.grl')
 
 
 def main():
